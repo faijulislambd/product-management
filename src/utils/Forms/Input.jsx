5e316e9bfc69dfcf039c583dd79763ebfda43afc
faijulislambd <f.faijulislam144@gmail.com>
2024-05-04 22:13:30 +0600

    Label Color Fixed

--- src/utils/Forms/Input.jsx
+++ src/utils/Forms/Input.jsx
@@ -16,7 +16,7 @@ const Input = ({
         className={`peer py-2 px-4 border border-slate-200 rounded-md text-lg ${width} ${shadow} focus:outline-none focus:border-slate-400`}
       />
       <label
-        className={`absolute top-9 left-4 pointer-events-none text-lg transition-all duration-300 text-slate-400 peer-focus:rounded-sm peer-focus:top-0 peer-focus:left-0 peer-focus:text-sm peer-focus:pointer-events-auto peer-focus:${labelColor}`}
+        className={`absolute top-9 left-4 pointer-events-none text-lg transition-all duration-300 text-slate-400 peer-focus:rounded-sm peer-focus:top-0 peer-focus:left-0 peer-focus:text-sm peer-focus:pointer-events-auto peer-focus:text-black`}
       >
         {label}
       </label>
